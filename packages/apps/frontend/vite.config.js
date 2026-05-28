aec7488ea0b34d1716ac2898ff80d0dc83416163
sk <alexxl1932@outlook.com>
2026-05-29 04:23:16 +0800

    chore(frontend): set up Tailwind

--- packages/apps/frontend/vite.config.js
+++ packages/apps/frontend/vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
